539577f0e102b90a7b94aae8ca35506be99308fe
ericpotter <26855591lai@gmail.com>
2018-01-09 20:23:52 +0800

    Signed-off-by: ericpotter <26855591lai@gmail.com>

---
+++ chickweight 成長體重.R
@@ -0,0 +1,5 @@
+# 繪製 plot 圖示 Chick的成長體重
+with(ChickWeight, plot(Time, weight, col = Diet,pch=20,ylab='Weight'))
+legend('topleft',legend=paste("Diet",levels(ChickWeight$Diet)),col=1:4,lwd=3)
+ChickWeight$Chick
+sort(table(ChickWeight$Chick))
